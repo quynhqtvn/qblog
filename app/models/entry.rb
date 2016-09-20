3839a8669d21e14314f83e994816cb50cbdb202c
Quynh Thai <thaiquynh.inbox@gmail.com>
2016-09-20 20:45:28 +0700

    add user_name into model Users and render on website

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -2,6 +2,6 @@ class Entry < ActiveRecord::Base
 	belongs_to :user
 	has_many :comments
 	validates :user_id, presence: true
-	validates :content, presence: true, length: { minimum: 30}
+	validates :content, presence: true
 	validates :title, presence: true
 end
